24a01b3d53a00a6a536dd47f5c6bb715431e2c59
sugiarto550 <sugibambang66@gmail.com>
2025-03-15 11:10:48 +0000

    perubahan

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Java JavaScript</title>
 // Tampilkan alert otomatis saat halaman dimuat
 window.onload = function() {
     alert("Selamat datang di website Bambang Sugiarto!");
